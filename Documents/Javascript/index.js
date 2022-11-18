f8ed6aaafa9b2513e06680ef2895ed2414d9fa2c
abdulkemp <aakemp786@gmail.com>
2022-11-18 14:14:34 +0200

    javascript

---
+++ Documents/Javascript/index.js
@@ -0,0 +1,173 @@
+/*let obj = {
+  cat: "meow",
+  dog: "woof",
+};
+
+let dog = "cat";
+//let sound = obj[dog]
+let sound = obj.dog;
+console.log(sound);
+
+let car = [(car1 = "toyota"), (car2 = "BMW"), (car3 = "Bugatti")];
+console.log(car[1]);
+
+const array = ["toy", "bmw", "tata"];
+
+console.log(typeof array);
+
+let points = 500;
+status = "bronze";
+
+if (points >= 500) {
+  status = "silver";
+  console.log(status);
+} else if (points >= 800) {
+  status = "gold";
+  console.log(status);
+}*/
+
+// let anything = 'apple';
+//     if (anything == 'strawberry') {
+//         console.log(anything);
+//     }
+//     else if (anything == 'apple') {
+//         anything = 'pear'
+//         console.log(anything);
+//     }
+//     else{
+//         console.log(anything);
+//     }
+
+/*var numberAsWords = [
+    'one',
+    'two',
+    'three'
+];
+
+console.log(numberAsWords);
+console.log(numberAsWords[0]);
+console.log(numberAsWords[1]);
+console.log(numberAsWords[2]);
+
+for (var i = 0 ; i < numberAsWords.length; i++){
+    console.log(numberAsWords[i]);
+};*/
+
+/*let score = prompt("what is your score?");
+let letter;
+
+if (score >= 90) {
+  letter = "A";
+  console.log(letter);
+} else if (score >= 80) {
+  letter = "B";
+  console.log(letter);
+} else if (score >= 70) {
+  letter = "C";
+  console.log(letter);
+} else if (score >= 60) {
+  letter = "D";
+  console.log(letter);
+} else {
+  letter = "F";
+  console.log(letter);
+}*/
+
+// const cars=[
+//     'toyota',
+//     'bmw',
+//     'vw'
+// ];
+// for (const vroom in cars) {
+//     console.log(vroom);
+// }
+
+// let person = {
+//     name: 'Ryan',
+//     surname: 'Thomas',
+//     shoeSize: 30
+// };
+// for (const people in person) {
+//     console.log(people);
+// }
+
+// let  x= 1;
+// while(x<21){
+//     console.log(x);
+//     x++;
+// }
+
+// let a = 10;
+// if (a == 10){
+//   a=52;
+//   console.log(a);
+// }
+// for (let i = 1; i < 21; i++){
+//   console.log(i);
+
+// if (i % 2 === 0) {
+//   document.write(i + " even number" + '<br/>');
+// } else {
+//   document.write(i + " odd number" + '<br/>');
+
+// }
+// }
+
+// const cars = [
+//   'toyota',
+//   'opel',
+//   'nissan',
+//   'subaru',
+//   'fiat'
+// ];
+
+// let x = 0;
+// for (let x = 0; x < 5; x++) {
+//   console.log("this car has an index of " + x)
+// }
+// while(x < 5){
+//   console.log()
+// }
+// let btn = document.querySelector('#btn');
+
+// btn.addEventListener('click', function() {
+//   console.log('Abdul');
+// });
+
+// function games(){
+//   let title = 'Zelda';
+//   console.log(title, greetings);
+// }
+
+// games();
+// console.log(greetings);
+
+// function myName(){
+//   console.log('Abdul');
+// }
+
+// myName();
+
+// let myName = prompt('What is your name?');
+// function greet(name){
+//   console.log('hello ' + name)
+// }
+
+// greet(myName);
+
+// function addIt(num1, num2){
+//   console.log(num1 + num-2)
+// };
+
+// addIt(1,2);
+
+//Task......
+
+// let na = prompt('What is your name?');
+// function greeting(na){
+//   alert('Hello ' + na )
+// }
+// greeting(na);
+
+// n = prompt("What is your name?");
+// if (n) alert("Hello " + n);
